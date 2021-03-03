db7da5ac3e66474013b1ab8f088a4383dd9c57ef
Paul Lee <paul_lee@kakao.com>
2021-03-03 19:18:37 +0900

    user, room grpahql

--- config/schema.py
+++ config/schema.py
@@ -1,22 +1,11 @@
 import graphene
-from graphene_django import DjangoObjectType
-from rooms.models import Room
+from rooms import schema as rooms_schema
+from users import schema as users_schema
 
 
-class RoomType(DjangoObjectType):
-    class Meta:
-        model = Room
+class Query(rooms_schema.Query, users_schema.Query, graphene.ObjectType):
+    pass
 
-
-class Query(graphene.ObjectType):
-    hello = graphene.String()
-    rooms = graphene.List(RoomType)
-
-    def resolve_hello(self, info):
-        return "Hello"
-
-    def resolve_rooms(self, info):
-        return Room.objects.all()
 
 class Mutation:
     pass
---
+++ users/schema.py
@@ -0,0 +1,10 @@
+import graphene
+from .types import UserType
+from .models import User
+
+
+class Query(object):
+    user = graphene.Field(UserType, id=graphene.Int(required=True))
+
+    def resolve_user(self, info, id):
+        return User.objects.get(id=id)
